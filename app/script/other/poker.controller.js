b92a450344da773cd72d7dd48f2478237f9115b1
xiaoliang2535 <cxj_liang@126.com>
2014-06-25 17:39:26 +0800

    add image

--- app/script/other/poker.controller.js
+++ app/script/other/poker.controller.js
@@ -6,30 +6,47 @@ $(function(){
   //var poker = new Poker();
 
   // 發牌
-  $(".m-total").delegate(".js-start-btn", "click", function(e) {
-    $(".schedule").width(14*51 + 60);
-    $(".m-user .poker").each(function(index) {
-      var offset = $(this).offset();
-      setTimeout(function(){
-        (new Poker(offset.left, offset.top)).init();
-      }, index*200);
-    });
+  $('.m-total').delegate('.js-start-btn', 'click', function(e) {
+
+    $('.schedule').width(14*51 + 60);
+
+    $('.poker').find("img").remove();
+    $('.poker').append('<img class="poker-image" />');
+    $('.js-open-btn').show();
+
+      $('.m-user .poker').each(function(index) {
+        var that = this;
+        setTimeout(function(){
+          (new Poker(that, 2)).init(true);
+        }, index*200);
+      });
 
     // 延迟右侧发牌时间
     setTimeout(function(){
-      $(".m-system .poker").each(function(index) {
-        if(index > 2) return;
-        var offset = $(this).offset();
+      $('.m-system .poker').each(function(index) {
+        var that = this;
         setTimeout(function(){
-          (new Poker(offset.left, offset.top)).init();
+          var flag = index > 1 ? false : true;
+          (new Poker(that, 1)).init(flag);
         }, index*200);
       });
     }, 1000);
 
   });
 
+  $('.m-user').delegate('.js-reset-btn', 'click', function(e) {
+    var that = $(this).siblings(".poker");
+    (new Poker(that, 1)).init(true);
+  });
+
+  $('.m-system').delegate('.js-open-btn', 'click', function(e) {
+    $(this).siblings(".poker").find('img').attr('src','../image/poker/tmp/'+1+'.jpg');
+    $(this).hide();
+  });
+
   // 開牌
-  $(".m-total").delegate(".js-end-btn", "click", function(e) {
+  $('.m-total').delegate('.js-end-btn', 'click', function(e) {
+    $(".js-open-btn").click();
   });
 
 });
--- app/script/other/poker.model.js
+++ app/script/other/poker.model.js
@@ -1,27 +1,29 @@
 // 移动扑克类
 
-function Poker(left, top) {
-  this.$rPoker = $("#right-poker");
-     this.left = left;
-      this.top = top;
+function Poker(dPoker, num) {
+  this.$rPoker = $('#right-poker');
+  this.$dPoker = $(dPoker);
+      this.num = num;
+     this.left = this.$dPoker.offset().left;
+      this.top = this.$dPoker.offset().top;
 }
 
-Poker.prototype.init = function() {
+Poker.prototype.init = function(flag) {
   this.createPoker();
   this.movePoker();
-  this.removePoker();
+  this.removePoker(flag);
 };
 
 Poker.prototype.createPoker = function() {
   var that = this;
-  that.$pPoker = $("<div id='pos-poker' class='pos-poker poker'></div>");
+  that.$pPoker = $('<div id="pos-poker" class="pos-poker poker"></div>');
   that.$rPoker.after(that.$pPoker);
   that.$pPoker.offset(that.$rPoker.offset());
 };
 
 Poker.prototype.movePoker = function() {
   var that = this;
-  $(".schedule").width($(".schedule").width()-14);
+  $('.schedule').width($('.schedule').width()-14);
   console.log();
   that.$pPoker.animate({
     left: that.left,
@@ -29,9 +31,12 @@ Poker.prototype.movePoker = function() {
   });
 };
 
-Poker.prototype.removePoker = function() {
+Poker.prototype.removePoker = function(flag) {
   var that = this;
   setTimeout(function(){
+    if(flag) {
+      that.$dPoker.find('img').attr('src', '../image/poker/tmp/'+that.num+'.jpg');
+    }
     that.$pPoker.remove();
   }, 1000);
 };
